5e59b0443e06b71ed0be460ef9d51f9fbaaa5c01
Hugo CREPIN <crepin.hugo30@gmail.com>
2026-02-03 21:24:12 +0100

    fix effet

--- dataset.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Effect (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440000', 'pv', 20, '+', 1);
-INSERT INTO Effect (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440001', 'attack', 5, '+', 2);
-INSERT INTO Effect (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440002', 'speed', 5, '-', 2);
-INSERT INTO Effect (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440003', 'speed', 15, '+', 2);
-INSERT INTO Effect (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440004', 'attack', 50, '+', 2);
-INSERT INTO Effect (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440005', 'attack', 10, '-', 3);
-INSERT INTO Effect (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440006', 'speed', 10, '-', 3);
-INSERT INTO Effect (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440007', 'pv', 40, '+', 2);
--- prisma/migrations/20260130081819_init/migration.sql
+++ prisma/migrations/20260130081819_init/migration.sql
@@ -9,3 +9,12 @@ CREATE TABLE `Effet` (
     INDEX `Effet_stat_name_idx`(`stat_name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+INSERT INTO Effet (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440000', 'pv', 20, '+', 1);
+INSERT INTO Effet (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440001', 'attack', 5, '+', 2);
+INSERT INTO Effet (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440002', 'speed', 5, '-', 2);
+INSERT INTO Effet (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440003', 'speed', 15, '+', 2);
+INSERT INTO Effet (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440004', 'attack', 50, '+', 2);
+INSERT INTO Effet (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440005', 'attack', 10, '-', 3);
+INSERT INTO Effet (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440006', 'speed', 10, '-', 3);
+INSERT INTO Effet (id, stat_name, count, modificator, duration) VALUES ('660e8400-e29b-41d4-a716-446655440007', 'pv', 40, '+', 2);
