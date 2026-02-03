4a533f568cc16b2fdc2eff8ea221953d09459e39
Hugo CREPIN <crepin.hugo30@gmail.com>
2026-02-03 21:11:47 +0100

    openapi.yml

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 
+import fs from 'node:fs';
+
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yaml';
 
 import { EffectController } from './controllers/effectController';
 import { EffectRouter } from './routes/effectRouter';
@@ -16,7 +20,14 @@ const effectService = new EffectService();
 const effectController = new EffectController(effectService);
 
 app.use('/effect', new EffectRouter(effectController).router);
+
+const file = fs.readFileSync('./openapi.yml', 'utf8');
+const swaggerDocument = YAML.parse(file) as object;
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`docs available at http://localhost:${port}/docs`);
 });
